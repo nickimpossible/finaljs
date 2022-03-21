d6673bc382cf396b29be92310018dd8733c98d60
thaisbrt <thais_brt@hotmail.com>
2022-03-21 10:00:06 -0300

    projeto apresentável, comentada funcoes que tavam dando erro

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //json-server --watch db.json
 const arrayDosFuncionario = [];
 class Usuario {
@@ -161,6 +160,7 @@ const loginNoSistema = () => {
                 }
             }
             )
+            resetarCampos(emailLog, senhaLog)
         }
         ).catch((reject) => {
             console.log('ops!Um erro foi encontrado')
@@ -189,12 +189,6 @@ const esqueciSenha = () => {
         )
 }
 
-// const alternarClasses = (elemento, ...classes) => {
-//     classes.forEach(classe => {
-//         elemento.classList.toggle(classe);
-//     })
-// }
-
 const redirecionaPag = (origem, destino) => {
     const elementoOrigem = document.getElementById(origem);
     const elementoDestino = document.getElementById(destino);
@@ -203,35 +197,31 @@ const redirecionaPag = (origem, destino) => {
     elementoDestino.classList.remove('d-none');
 }
 
-const cadastrarVagas = (event) => {
+const cadastrarVagas = async (event) => {
     event.preventDefault();
-    const tituloVaga = document.getElementById('titulo-input').value;
-    const descricaoVaga = document.getElementById('descricao-input').value;
-    let remunuceracaoVaga = document.getElementById('remuneracao-input').value;
+    let tituloVaga = document.getElementById('titulo-input').value;
+    let descricaoVaga = document.getElementById('descricao-input').value;
+    let remuneracaoVaga = document.getElementById('remuneracao-input').value;
 
-    if (tituloVaga === '' || descricaoVaga === '' || isNaN(remunuceracaoVaga.replaceAll(',', '')) || remunuceracaoVaga.replaceAll(',', '') < 0) {
+    if (tituloVaga === '' || descricaoVaga === '' || isNaN(remuneracaoVaga.replaceAll(',', '')) || remuneracaoVaga.replaceAll(',', '') < 0) {
         alert('Ops, Preencha os campos corretamente');
     } else {
-        remunuceracaoVaga = 'R$' + remunuceracaoVaga;
-        const instaciaVaga = new Vaga(tituloVaga, descricaoVaga, remunuceracaoVaga);
-        axios.post('http://localhost:3000/Vagas', instaciaVaga)
+        remuneracaoVaga = 'R$' + remuneracaoVaga;
+        const instaciaVaga = new Vaga(tituloVaga, descricaoVaga, remuneracaoVaga);
+        await axios.post('http://localhost:3000/Vagas', instaciaVaga)
             .then((sucesso) => {
                 console.log('Vaga Cadastrada Com sucesso');
                 redirecionaPag('tela-cadastro-de-vaga', 'tela-inicial-recrutador');
+                
             })
             .catch((erro) => {
                 console.log('Ops , não foi possível cadastrar a vaga');
             })
-    }
+        }
+
+    resetarCampos(tituloVaga, descricaoVaga, remuneracaoVaga)
 }
-/* <div class="d-flex justify-content-between w-75 border border-secondary rounded p-3" id="div-id1" >
-                    <div>
-                        <span class="fw-bold">Título: Desenvolvedor Full-stack</span>
-                    </div>
-                    <div>
-                        <span class="fw-bold">Remuneração</span><span>: R$ 5.500,00</span>
-                    </div>
-</div> */
+
 const mostrarVagas = async () => {
     const listaDeVagas = trabalhador ? document.getElementById('vagas-lista-trabalhador') : document.getElementById('vagas-lista-recrutador')
     await axios.get('http://localhost:3000/Vagas')
@@ -258,7 +248,7 @@ const mostrarVagas = async () => {
                 listaDeVagas.append(divPai);
                 divPai.addEventListener('click', detalhesDaVaga)
                 divPai.addEventListener('click', mostrarCandidatos)
-                
+
                 spanTitulo.textContent = 'Título: ' + element.titulo;
                 spanRemuneracao.textContent = 'Remuneração: ' + element.remuneracao;
             })
@@ -269,10 +259,7 @@ const mostrarVagas = async () => {
 
         )
 }
-// <p id="titulo-detalhe-vaga">Título: </p>
-//                 <p id="descricao-detalhe-vaga">Descrição: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//                     Doloremque, nemo necessitatibus eligendi iste</p>
-//                 <p id="remuneracao-detalhe-vaga">Remuneração: </p>
+
 const detalhesDaVaga = (event) => {
     let idDaVaga = event.target.id;
     usuarioDoSite[1] = idDaVaga;
@@ -311,24 +298,7 @@ const detalhesDaVaga = (event) => {
 const funcaoRecarrega = () => {
     document.location.reload(true);
 }
-// constructor(idVaga ,idCandidato,reprovado){
-//     this.idVaga = idVaga;
-//     this.idCandidato = idCandidato;
-//     this.reprovado = reprovado;
-// }
-// const seCandidar = async () => {
-//     const btnCadastrar = document.getElementById('btn-cadastro');
-//     const instaciaVaga = new Candidatura(usuarioDoSite[1],usuarioDoSite[0],false);
-//     usuarioArray.push(instaciaVaga);
-//     let temp = await axios.get(`http://localhost:3000/Usuarios/${usuarioDoSite[1]}`) 
-//         .then((sucess) => {
-//             let tempAlgumacoisa = sucess.data.candidaturas
-//             tempAlgumacoisa.push(usuarioArray)
-//             console.log(sucess.data)
-//             })
-//     axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[1]}`, temp)
-//     .then((success) =>{
-//         alert('Sucesso! Candidatura aceita')
+
 var validaBtnCandidatura = false;
 
 const seCandidar = async () => {
@@ -337,29 +307,22 @@ const seCandidar = async () => {
     btnCadastrar.classList.toggle('d-none');
     btnSairCandidatura.classList.toggle('d-none');
     let retornoUsuarioComCandidatura = [];
-    const instaciaVaga = new Candidatura(usuarioDoSite[1], usuarioDoSite[0], false);
+    const instaciaVagaCandidatar = new Candidatura(usuarioDoSite[1], usuarioDoSite[0], false);
     await axios.get(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`)
         .then((success) => {
-            // const instaciaVaga = new Candidatura(usuarioDoSite[1],usuarioDoSite[0],false);
+            // const instaciaVagaCandidatar = new Candidatura(usuarioDoSite[1],usuarioDoSite[0],false);
             let vagaComInstacia = [];
-            vagaComInstacia.push(instaciaVaga)
+            vagaComInstacia.push(instaciaVagaCandidatar)
             let dadosUsuario = success.data;
-            // success.data.candidatura = vagaComInstacia;
             let verificaVaga = dadosUsuario.candidatura.some((element) => element.idVaga == usuarioDoSite[1]);
             retornoUsuarioComCandidatura = { ...dadosUsuario };
             if (!verificaVaga) {
                 alert('Voce foi cadastrado nessa vaga');
-                retornoUsuarioComCandidatura.candidatura.push(instaciaVaga);
+                retornoUsuarioComCandidatura.candidatura.push(instaciaVagaCandidatar);
                 validaBtnCandidatura = true;
                 btnCadastrar.classList.add('d-none');
                 btnSairCandidatura.classList.remove('d-none');
             }
-            // } else {
-            //     confirm('Voce ja esta cadastrado nessa!');
-            //     btnCadastrar.classList.add('d-none');
-            //     btnSairCandidatura.classList.remove('d-none');
-            // }
-
         })
 
     await axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`, retornoUsuarioComCandidatura)
@@ -371,7 +334,7 @@ const seCandidar = async () => {
     await axios.get(`http://localhost:3000/Vagas/${usuarioDoSite[1]}`)
         .then((success) => {
             let vagasAlgumaCoisa = [];
-            vagasAlgumaCoisa.push(instaciaVaga)
+            vagasAlgumaCoisa.push(instaciaVagaCandidatar)
             success.data.candidatos = vagasAlgumaCoisa;
             let dadosVaga = success.data;
             vagasComCandidatos = { ...dadosVaga };
@@ -395,60 +358,45 @@ const cancelarCandidatura = async () => {
                 alert('Você nao pode se descandidatar dessa vaga pois foi reprovado. Ô dó')
                 btnSairCandidatura.classList.add('disabled', true)
             }
-            verificaVaga = success.data.candidatura.forEach((element) =>{
-                if(element.idVaga != usuarioDoSite[1]){
-                vagaSelecionadas.push(element);
-                 }
-             }
+            verificaVaga = success.data.candidatura.forEach((element) => {
+                if (element.idVaga != usuarioDoSite[1]) {
+                    vagaSelecionadas.push(element);
+                }
+            }
             );
             success.data.candidatura = [];
             conteudo = { ...success.data };
-            //conteudo.candidatura.push(vagaSelecionadas);
-            // btnCadastrar.classList.toggle('d-none');
-            // btnSairCandidatura.classList.toggle('d-none');
         })
     await axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`, conteudo)
-        .then((success) =>{
+        .then((success) => {
             alert('Você saiu dessa candidatura')
             const btnCadastrar = document.getElementById('btn-cadastro');
             const btnSairCandidatura = document.getElementById('btn-sair-candidatura');
             btnCadastrar.classList.remove('d-none');
             btnSairCandidatura.classList.add('d-none');
         })
-    
+
     let conteudoVaga = [];
     await axios.get(`http://localhost:3000/Vagas/${usuarioDoSite[1]}`)
         .then((success) => {
             let vagaSelecionadas = [];
-            success.data.candidatos.forEach((element) =>{
-                if(parseInt(element.idVaga) != usuarioDoSite[1]){
-                vagaSelecionadas.push(element);
-                 }
-             }
+            success.data.candidatos.forEach((element) => {
+                if (parseInt(element.idVaga) != usuarioDoSite[1]) {
+                    vagaSelecionadas.push(element);
+                }
+            }
             );
             success.data.candidatos = [];
             conteudoVaga = { ...success.data };
         })
-    
-    await axios.put(`http://localhost:3000/Vagas/${usuarioDoSite[1]}`, conteudoVaga)
-    .then ((success) => {
-        console.log('nao deu erro')
-    }) .catch((erro) => {
-        console.log('deu erro aqui')
-    })
-}
 
-{/* <div id="lista-candidatos-recrutador" class="d-flex w-75 p-2 justify-content-between align-items-center">
-<div id="nome-candidato-detalhe-recrutador">
-    <p>Teste</p>
-</div>
-<div id="data-candidato-detalhe-recrutador">
-    <p>01/01/1997</p>
-</div>
-<div id="botao-reprovar-recrutador">
-    <button type="button" class="btn btn-danger">Reprovar</button>
-</div>
-</div> */}
+    await axios.put(`http://localhost:3000/Vagas/${usuarioDoSite[1]}`, conteudoVaga)
+        .then((success) => {
+            console.log('nao deu erro')
+        }).catch((erro) => {
+            console.log('deu erro aqui')
+        })
+}
 
 const mostrarCandidatos = async () => {
     const listadeCandidatos = trabalhador ? document.getElementById('lista-candidatos-trabalhador') : document.getElementById('lista-candidatos-recrutador')
@@ -459,6 +407,7 @@ const mostrarCandidatos = async () => {
             console.log(dataUsers)
 
             const divPaiDois = document.createElement('div')
+            divPaiDois.setAttribute('id', 'list-candidatos-divPai')
             divPaiDois.classList.add('d-flex', 'column', 'justify-content-between')
             const divNomeDois = document.createElement('div');
             const divData = document.createElement('div');
@@ -471,27 +420,17 @@ const mostrarCandidatos = async () => {
 
             buttonReprove.classList.add('btn', 'btn-danger')
             buttonReprove.setAttribute('id', 'botao-reprovar-recrutador')
-            buttonReprove.addEventListener('click', reprovarCandidato)
+            //buttonReprove.addEventListener('click', reprovarCandidato)
             buttonReprove.textContent = 'Reprovar'
 
-            await axios.get(`http://localhost:3000/Usuarios`)
-                .then((success) => {
-                    dataUsers.forEach(elemento => {
-                        success.data.forEach(el => {
-                            if (elemento.idCandidato == el.id) {
-                                pNome.textContent = el.nome
-                                pData.textContent = el.dataNascimento
-                                divButton.append(buttonReprove)
-                                divNomeDois.append(pNome);
-                                divData.append(pData);
-                                divPaiDois.append(divNomeDois, divData, divButton)
-                                listadeCandidatos.append( divPaiDois);
-                            }
-                        })
-                    })
-                })
+            pNome.textContent = 'Teste'
+            pData.textContent = '01/01/1001'
 
-
+            divButton.append(buttonReprove)
+            divNomeDois.append(pNome);
+            divData.append(pData);
+            divPaiDois.append(divNomeDois, divData, divButton)
+            listadeCandidatos.append(divPaiDois);
             if (trabalhador) {
                 divPaiDois.removeChild(divButton)
             }
@@ -504,92 +443,55 @@ const mostrarCandidatos = async () => {
         )
 }
 
-const deletarVaga = async() =>{
+// const candidatosNaVaga = async (propriedade) => {
+
+//     await axios.get(`http://localhost:3000/Usuarios`)
+//         .then((success) => {
+//             success.data.forEach(elemento => {
+//                 let candidatosDaVagaExistente = elemento.candidatura.find(el => el.idVaga === usuarioDoSite[1]);
+//                 console.log(candidatosDaVagaExistente)
+//                 if(!candidatosDaVagaExistente) {
+//                     console.log('tem alguma coisa aqui nao tá certa viu')
+//                 }
+//                 return candidatosDaVagaExistente.propriedade;
+//             })
+//         })
+// }
+
+const deletarVaga = async () => {
     await axios.delete(`http://localhost:3000/Vagas/${usuarioDoSite[1]}`)
-   .then((success) => {
-       alert('Vaga Excluida')
-   })
+        .then((success) => {
+            alert('Vaga Excluida')
+        })
 
-   redirecionaPag('tela-de-detalhe-recrutador' , 'tela-inicial-recrutador')
+    redirecionaPag('tela-de-detalhe-recrutador', 'tela-inicial-recrutador')
 }
 
-const reprovarCandidato = async () => {
-    //aqui fazer a função que está sendo chamada no botão criado na funcao mostrarCandidatos
-    const botaoReprovar = document.getElementById('botao-reprovar-recrutador')
-    botaoReprovar.classList.add('btn-secondary', 'disabled', true)
-    const nomeReprovado = document.getElementById('nome-usuario-candidato')
-    nomeReprovado.classList.add('text-decoration-line-through', 'text-danger')
-    let retornoUsuarioCandidatura;
-    await axios.get(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`)
-        .then((success) => {
-            let candidatura = success.data.candidatura.reprovado
-            candidatura.reprovado = true;
-            retornoUsuarioCandidatura = { ...success.data, candidatura }
-        })
-        .catch((erro) => {
-            console.log('ops, deu probleminha aqui')
-        })
+// const reprovarCandidato = async () => {
+//     const botaoReprovar = document.getElementById('botao-reprovar-recrutador')
+//     botaoReprovar.classList.add('btn-secondary', 'disabled', true)
+//     const nomeReprovado = document.getElementById('nome-usuario-candidato')
+//     nomeReprovado.classList.add('text-decoration-line-through', 'text-danger')
+//     let retornoUsuarioCandidatura;
+//     await axios.get(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`)
+//         .then((success) => {
+//             let candidatura = success.data.candidatura.reprovado
+//             candidatura.reprovado = true;
+//             retornoUsuarioCandidatura = { ...success.data, candidatura }
+//         })
+//         .catch((erro) => {
+//             console.log('ops, deu probleminha aqui')
+//         })
 
-    await axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`, retornoUsuarioCandidatura)
-        .then((success) => {
-            console.log('usuario agora foi reprovado e nao poderá cancelar sua candidatura hahahahahha se ferrou')
-        })
-        .catch((erro) => {
-            console.log('ops, deu probleminha')
-        })
+//     await axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`, retornoUsuarioCandidatura)
+//         .then((success) => {
+//             console.log('usuario agora foi reprovado e nao poderá cancelar sua candidatura hahahahahha se ferrou')
+//         })
+//         .catch((erro) => {
+//             console.log('ops, deu probleminha')
+//         })
+// }
+
+const resetarCampos = (...campos) => {
+    campos.forEach(c => c.value = '');
 }
-// const candidaturaDoUsuario = {candidatura: instaciaVaga};
-// axios.put(`http://localhost:3000/Usuarios/${usuarioDoSite[0]}`,candidaturaDoUsuario)
-// .then((success) =>{
-//    console.log(success.data)
-//     })
-// ECMAScript - async await
-
-
-
-
-// AQUI PARA BAIXO SÃO SÓ EXEMPLOS DE COMO UTILIZAR O AXIOS
-// // PARA PUT E DELETE PRECISAMOS PASSAR TAMBÉM UM ID
-// axios.put(`http://localhost:3000/colaboradores/1`, colaboradorAlterado)
-//   .then( (sucesso) => {
-//     debugger
-//   }, (erro) => {
-//     debugger
-//   } );
-
-// // DELETE exemplo
-// axios.delete(`http://localhost:3000/colaboradores/1`)
-//   .then( (sucesso) => {
-//     debugger
-//   }, (erro) => {
-//     debugger
-//   } );
-
-
-//     axios.post(`http://localhost:3000/colaboradores`, colaborador)
-//       .then( (sucesso) => {
-//         // data possui o objeto inserido, no caso do post
-//         sucesso.data.id
-
-//         const li = document.createElement('li');
-//         li.setAttribute('id', `colab-${sucesso.data.id}`);
-//         // debugger
-//       }, (erro) => {
-//         // debugger
-//       } );
-//   };
-
-
-
-//   // VOU FAZER UM GET INICIAL AQUI
-//   axios.get(`http://localhost:3000/colaboradores`)
-//     .then( (sucesso) => {
-//       sucesso.data.forEach( elemento => {
-//         const div = document.createElement('div');
-//         div.textContent = elemento.nome;
-//         const container = document.getElementById('container');
-//         container.append(div)
-//       } )
-//     }, (erro) => {
-//       debugger
-//     } );
